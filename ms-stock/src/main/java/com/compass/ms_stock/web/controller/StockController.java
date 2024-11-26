8d5a1b08f3ebd0c8c59adccc8f8f9b1c95bad0b9
Eduardo Decarli <decarliedu@gmail.com>
2024-11-26 19:16:09 -0300

    :sparkles: feat: Convert swagger annotations for file .yaml

--- ms-stock/src/main/java/com/compass/ms_stock/web/controller/StockController.java
+++ ms-stock/src/main/java/com/compass/ms_stock/web/controller/StockController.java
@@ -25,76 +25,42 @@ public class StockController {
 
     private final StockService stockService;
 
-    @Operation(summary = "Create a new product", description = "Add a new product to the stock")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "Product successfully created"),
-            @ApiResponse(responseCode = "400", description = "Invalid product data")
-    })
     @PostMapping
     public ResponseEntity<String> createProduct(@RequestBody ProductCreateDTO create) {
         stockService.createProductInStock(create);
         return ResponseEntity.status(HttpStatus.CREATED).body("successfully create product");
     }
 
-    @Operation(summary = "Get all products", description = "Return a list of all products in the stock")
-    @ApiResponse(responseCode = "200", description = "List all products successfully")
     @GetMapping
     public ResponseEntity<List<ProductResponseDTO>> findAllProducts() {
         List<ProductResponseDTO> products = stockService.findAllProducts();
         return ResponseEntity.ok(products);
     }
 
-    @Operation(summary = "Get product by ID", description = "Retrieves a product by ID")
-    @Parameter(name = "id", description = "ID of the product", required = true)
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Product retrieved successfully"),
-            @ApiResponse(responseCode = "404", description = "Product not found")
-    })
     @GetMapping("/product/id/{id}")
     public ResponseEntity<ProductResponseDTO> findProductById(@PathVariable Long id) {
         Product product = stockService.findProductById(id);
         return ResponseEntity.ok(StockDTO.toDto(product));
     }
 
-    @Operation(summary = "find product by name", description = "Retrieves a product by name")
-    @Parameter(name = "name", description = "Name of the product", required = true)
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Product retrieved successfully"),
-            @ApiResponse(responseCode = "404", description = "Product not found")
-    })
     @GetMapping("/product/name/{name}")
     public ResponseEntity<ProductResponseDTO> findProductByName(@PathVariable String name) {
         Product product = stockService.findProductByName(name);
         return ResponseEntity.ok(StockDTO.toDto(product));
     }
 
-    @Operation(summary = "Update a product", description = "Updates the details of an existing product")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "Product successfully updated"),
-            @ApiResponse(responseCode = "400", description = "Invalid data")
-    })
     @PutMapping("/{id}")
     public ResponseEntity<String> updateProduct(@RequestBody ProductCreateDTO update, @PathVariable Long id) {
         stockService.updateProductInStock(update, id);
         return ResponseEntity.status(HttpStatus.CREATED).body("successfully update product");
     }
 
-    @Operation(summary = "Remove product quantity", description = "Removes a specified quantity from a product in stock")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Quantity removed successfully"),
-            @ApiResponse(responseCode = "404", description = "Product not found")
-    })
     @GetMapping("/quantity/remove/{quantity}/{name}")
     public ResponseEntity<String> removeQuantity(@PathVariable Integer quantity, @PathVariable String name) {
         stockService.removeQuantityByName(quantity, name);
         return ResponseEntity.ok().body("Removed quantity successfully");
     }
 
-    @Operation(summary = "Add product quantity", description = "Add a specified quantity to a product in stock")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Quantity added successfully"),
-            @ApiResponse(responseCode = "404", description = "Product not found")
-    })
     @GetMapping("/quantity/add/{quantity}/{name}")
     public ResponseEntity<String> addQuantity(@PathVariable Integer quantity, @PathVariable String name) {
         stockService.addQuantityByName(quantity, name);
