9ec09cbf864b7dcac2c21ce49434dea93e365ccd
Eduardo Decarli <decarliedu@gmail.com>
2024-11-27 09:54:31 -0300

    :sparkles: feat: Implemets Swagger Annotations for OrdersController

--- ms-order/src/main/java/com/compass/ms_order/web/controller/OrderController.java
+++ ms-order/src/main/java/com/compass/ms_order/web/controller/OrderController.java
@@ -2,6 +2,11 @@ package com.compass.ms_order.web.controller;
 
 import com.compass.ms_order.entities.Order;
 import com.compass.ms_order.services.OrderServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +21,22 @@ public class OrderController {
 
     private final OrderServices services;
 
-    @PostMapping
+    @Operation(summary = "Create a new order", description = "Creates a new order in the system.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Order created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
+    @PostMappin
     public ResponseEntity<Order> createOrder(@RequestBody Order create) {
         Order order = services.createOrder(create);
         return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 
+    @Operation(summary = "Find orders by email", description = "Fetches all orders associated with a email.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Orders retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "No orders found for the email")
+    })
     @GetMapping("historic/{email}")
     public ResponseEntity<List<Order>> findOrdersByEmail(@PathVariable String email) {
         List<Order> order = services.findAllOrderByEmail(email);
