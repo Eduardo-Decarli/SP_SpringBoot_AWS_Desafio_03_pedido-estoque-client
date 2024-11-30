cc27709b137c3eafd167580b8f7149dd83068198
Eduardo Decarli <decarliedu@gmail.com>
2024-11-30 12:41:45 -0300

    :sparkles: feat: Docker-compose and dockerfile for all project

--- ms-order/src/main/java/com/compass/ms_order/web/controller/clients/StockClient.java
+++ ms-order/src/main/java/com/compass/ms_order/web/controller/clients/StockClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "stock-services", url = "http://localhost:8080/api/v1/stock")
+@FeignClient(name = "stock-services", url = "http://ms-stock:8080/api/v1/stock")
 public interface StockClient {
 
     @GetMapping("/quantity/remove/{quantity}/{name}")
