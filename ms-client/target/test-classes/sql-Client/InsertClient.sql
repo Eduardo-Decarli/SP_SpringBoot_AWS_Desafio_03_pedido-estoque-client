7c9f7506ff9b58bb4db20c6eea20a76dfaeae1c1
Eduardo Decarli <decarliedu@gmail.com>
2024-12-02 12:27:55 -0300

    :sparkles: feat: Create docker-compose by all ms

---
+++ ms-client/target/test-classes/sql-Client/InsertClient.sql
@@ -0,0 +1,3 @@
+INSERT INTO Client (name, email) VALUES ('João Silva', 'JOAO.SILVA@EXAMPLE.COM');
+INSERT INTO Client (name, email) VALUES ('Maria Oliveira', 'MARIA.OLIVEIRA@EXAMPLE.COM');
+INSERT INTO Client (name, email) VALUES ('Carlos Santos', 'CARLOS.SANTOS@EXAMPLE.COM');
---
+++ ms-order/target/test-classes/sql-Order/InsertOrder.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO orders (email_clients, protocol) VALUES ('cristiane@example.com', '123');
+INSERT INTO orders (email_clients, protocol) VALUES ('pedro@example.com', '321');
+
+INSERT INTO product (order_id, name, quantity) VALUES (1, 'Smartphone', 10);
+INSERT INTO product (order_id, name, quantity) VALUES (1, 'Teclado', 5);
+INSERT INTO product (order_id, name, quantity) VALUES (2, 'Mouse', 20);
+INSERT INTO product (order_id, name, quantity) VALUES (2, 'Laptop', 15);
